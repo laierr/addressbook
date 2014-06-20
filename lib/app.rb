34d98da920c256bff5b984dc0c4278126f561052
laierr <kenderberator@gmail.com>
2014-06-20 21:12:06 +0300

    Modular style

--- lib/app.rb
+++ lib/app.rb
@@ -1,17 +1,19 @@
-require 'sinatra'
+require 'sinatra/base'
 require_relative "./addressbook"
 
 ADDRESSBOOK = Addressbook.new
 ADDRESSBOOK.put('Ololosha', 'Somestreet, 256')
 ADDRESSBOOK.put('Nyasha', 'Someother street, 64')
 
-get '/' do
-  erb :index
-end
+class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
-get '/person/:name' do   
-  name = params[:name]
-  address = ADDRESSBOOK.get(name)
+  get '/person/:name' do
+    name = params[:name]
+    address = ADDRESSBOOK.get(name)
 
-  erb :person, locals: {name: name, address: address}
+    erb :person, locals: {name: name, address: address}
+  end
 end
